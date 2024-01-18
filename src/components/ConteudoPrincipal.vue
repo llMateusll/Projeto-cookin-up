5474a91dd886b05eb36c671fec7d6f44de470ee2
llMateusll <mateus.d.santana.99@gmail.com>
2024-01-18 15:12:16 -0300

    Colocando botão buscar para funcionar.

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -3,12 +3,16 @@ import SelecionarIngredientes from './SelecionarIngredientes.vue';
 import SuaLista from './SuaLista.vue';
 import Tag from './Tag.vue';
 
+type Pagina = 'SelecionarIngredientes' | 'MostrarReceitas';
+
 export default {
   data() {
     return {
-      ingredientes: [] as string[]
+      ingredientes: [] as string[],
+      conteudo: 'SelecionarIngredientes' as Pagina
     };
   },
+
   components: { SelecionarIngredientes, Tag, SuaLista },
   methods: {
     adicionarIngrediente(ingrediente: string) {
@@ -17,6 +21,9 @@ export default {
     removerIngrediente(ingrediente: string) {
       this.ingredientes = this.ingredientes.filter(iLista => ingrediente !== iLista);
     },
+    navegar(pagina: Pagina){
+      this.conteudo = pagina
+    }
   }
 }
 </script>
@@ -25,10 +32,8 @@ export default {
   <main class="conteudo-principal">
     <SuaLista :ingredientes="ingredientes" />
 
-    <SelecionarIngredientes
-      @adicionar-ingrediente="adicionarIngrediente"
-      @remover-ingrediente="removerIngrediente"
-    />
+    <SelecionarIngredientes v-if="conteudo === 'SelecionarIngredientes'" @adicionar-ingrediente="adicionarIngrediente"
+      @remover-ingrediente="removerIngrediente" @buscar-receitas="navegar('MostrarReceitas')" />
   </main>
 </template>
 
